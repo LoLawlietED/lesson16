6ef42fd8517ba855274c7a299f06d82398e0aabb
LoLawlietED <stivsasha@gmail.com>
2024-09-01 13:16:12 +0700

    Add files via upload

--- Пространство имен2.py
+++ Пространство имен2.py
@@ -1,8 +1,11 @@
 def test_function():
     def inner_function():
         print("Я в области видимости функции test_function")
-        return
     inner_function()
-    return
 
-inner_function()
+
+
+test_function()   # выводит на печать строку: 'Я в области видимости функции test_function'
+
+inner_function()    # ошибка: NameError: name 'inner_function' is not defined.
+                    # Did you mean: 'test_function'?
